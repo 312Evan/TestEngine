f9e1faf9a26da12a52416627869151e95e49428e
Evan McGrael <evan@evans-laptop.myfiosgateway.com>
2024-04-13 14:46:11 -0400

    some shadow stuff but not perfect yet

--- src/main/java/enginetest/EngineFunctions/LightingManager.java
+++ src/main/java/enginetest/EngineFunctions/LightingManager.java
@@ -4,9 +4,10 @@ import com.jme3.app.SimpleApplication;
 import com.jme3.light.DirectionalLight;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
-import com.jme3.shadow.CompareMode;
+import com.jme3.post.FilterPostProcessor;
+import com.jme3.post.ssao.SSAOFilter;
+import com.jme3.shadow.DirectionalLightShadowFilter;
 import com.jme3.shadow.DirectionalLightShadowRenderer;
-import com.jme3.renderer.queue.RenderQueue;
 
 public class LightingManager {
 
@@ -19,16 +20,22 @@ public class LightingManager {
     public void addSun(ColorRGBA sunColor) {
         DirectionalLight sun = new DirectionalLight();
         sun.setColor(sunColor);
-        sun.setDirection(new Vector3f(100f, -100f, 0.5f).normalizeLocal());
+        sun.setDirection(new Vector3f(100f, -75f, 0.5f).normalizeLocal());
         app.getRootNode().addLight(sun);
 
-        int shadowMapSize = 1024;
-        int shadowMapQuality = 2;
-        DirectionalLightShadowRenderer dlsr = new DirectionalLightShadowRenderer(app.getAssetManager(), shadowMapSize, shadowMapQuality);
-        dlsr.setShadowIntensity(0.5f);
-        dlsr.setShadowCompareMode(CompareMode.Hardware);
+        final int SHADOWMAP_SIZE=1024;
+        DirectionalLightShadowRenderer dlsr = new DirectionalLightShadowRenderer(app.getAssetManager(), SHADOWMAP_SIZE, 3);
         dlsr.setLight(sun);
         app.getViewPort().addProcessor(dlsr);
-        app.getRootNode().setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
+
+        DirectionalLightShadowFilter dlsf = new DirectionalLightShadowFilter(app.getAssetManager(), SHADOWMAP_SIZE, 3);
+        dlsf.setLight(sun);
+        dlsf.setEnabled(true);
+        FilterPostProcessor fpp = new FilterPostProcessor(app.getAssetManager());
+        fpp.addFilter(dlsf);
+
+        SSAOFilter ssaoFilter = new SSAOFilter(0.3f, 1000f, 0.2f, 0.35f);
+        fpp.addFilter(ssaoFilter);
+        app.getViewPort().addProcessor(fpp);
     }
 }
